63cca5efd9637fa8c8a70376a34ef76c47f6b711
Mayra Alvarez <mayra.alvarez.velasquez@gmail.com>
2016-08-25 16:07:03 -0500

    agregando archivos

---
+++ app.js
@@ -0,0 +1,7 @@
+
+	var tiempo = parseInt(prompt("ingrese el tiempo"));
+	var costominuto = parseInt(prompt("ingrese el costo")); 
+	var horas = 0;
+	costo = (tiempo * costominuto);
+	document.write("el : ");
+	document.write(costo);  
